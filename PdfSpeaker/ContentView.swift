9b312861a6dd5f8599b5f9137763bb0bd8bc4124
muzammil <raajadobemania@gmail.com>
2024-10-23 12:19:49 +0500

    Initial Commit

---
+++ PdfSpeaker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PdfSpeaker
+//
+//  Created by Raaj's mac mini on 23/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PdfSpeaker/PdfSpeakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PdfSpeakerApp.swift
+//  PdfSpeaker
+//
+//  Created by Raaj's mac mini on 23/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PdfSpeakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
